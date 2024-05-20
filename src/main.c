8eb3a86f528fccee1c6a244ab0a770c9f170b120
amy <fabianpaci@protonmail.com>
2024-05-20 18:53:14 +1000

    inverse

--- src/main.c
+++ src/main.c
@@ -5,31 +5,47 @@
 #include <stdio.h>
 
 #define AMOUNT 20
+#define LENGTH 10
 #define SEGMENT(start, length, child) (Segment){start, {0.0f, 0.0f}, length, 0.0f, 0.0f, child}
 
 typedef struct Segment {
     Vector2 start;
     Vector2 end;    // "private"
     size_t length;
-    float absolute_angle;   // absolue angle. inherits from parent
-    float angle;            // relative angle. unique & disconnected
+    double absolute_angle;   // absolue angle. inherits from parent
+    double angle;            // relative angle. unique & disconnected
     struct Segment *child;
 } Segment;
 
-void update_segment(Segment *segment) {
-    segment->angle += 0.001*sin(GetTime());
+void follow(Segment *segment, Vector2 target) {
+    Vector2 direction = Vector2Subtract(target, segment->start);
 
-    float theta = segment->angle + segment->absolute_angle;
+    segment->angle = atan2(direction.y, direction.x);
+
+    direction = Vector2Normalize(direction);
+    direction = Vector2Scale(direction, segment->length);
+    direction = Vector2Scale(direction, -1);
+    segment->start = Vector2Add(target, direction);
+}
+
+void calculate_end(Segment *segment) {
+    double theta = segment->angle + segment->absolute_angle;
     Vector2 delta_line = {
         segment->length * cos(theta),
         segment->length * sin(theta),
     };
     segment->end = Vector2Add(segment->start, delta_line);
+    // segment->angle = theta;
+}
 
+void update_segment(Segment *segment) {
     if(segment->child != NULL) {
-        segment->child->absolute_angle = theta;
-        segment->child->start = segment->end;
+        follow(segment, segment->child->start);
+    } else {
+        follow(segment, GetMousePosition());
     }
+
+    calculate_end(segment);
 }
 
 void draw_segments(Segment *segment, size_t amount) {
@@ -42,12 +58,10 @@ int main(void) {
     InitWindow(800, 600, "kinematics");
     SetTargetFPS(60);
 
-    const Vector2 origin = {(float)GetScreenWidth()/2, (float)GetScreenHeight()/2};
-
     Segment segments[AMOUNT];
-    segments[0] = SEGMENT(origin, 50, NULL);
-    for(size_t i = 1; i < AMOUNT; i++) {
-        segments[i] = SEGMENT(origin, 50, &segments[i-1]);
+    segments[0] = SEGMENT(Vector2Zero(), LENGTH, NULL);
+    for(size_t i = 1; i < AMOUNT; i++)
+        segments[i] = SEGMENT(Vector2Zero(), LENGTH, &segments[i-1]);
 
     while(!WindowShouldClose()) {
         for(size_t i = 0; i < AMOUNT; i++)
